901ec763bfb90bab95cb48388f9fac7fe4b1d045
Alone <hi@anlo.ng>
2020-11-29 18:28:50 +0800

    Support for PHP 8

--- src/EloquentBuilder.php
+++ src/EloquentBuilder.php
@@ -13,27 +13,34 @@ class EloquentBuilder extends Builder
      * @param  array     $models
      * @param  string    $name  relationship name
      * @param  \Closure  $constraints
+     *
      * @return array
      */
-    protected function eagerLoadRelation(array $models,$name,\Closure $constraints)
+    protected function eagerLoadRelation(array $models, $name, \Closure $constraints)
     {
+        // First we will "back up" the existing where conditions on the query so we can
+        // add our eager constraints. Then we will merge the wheres that were on the
+        // query back to it in order that any where conditions might be specified.
         $relation = $this->getRelation($name);
         $relation->addEagerConstraints($models);
+
         $constraints($relation);
-        $models = $relation->initRelation($models,$name);
+
+        $models = $relation->initRelation($models, $name);
         $parent = $relation->getParent();
 
         $results = null;
-        if(method_exists($parent,'eagerLoadRelationFromModel'))
-        {
-            $results = $parent->eagerLoadRelationFromModel($relation,$name,$models);
+        if(method_exists($parent, 'eagerLoadRelationFromModel')) {
+            $results = $parent->eagerLoadRelationFromModel($relation, $name, $models);
         }
-        if(is_null($results))
-        {
+        if(is_null($results)) {
             $results = $relation->getEager();
         }
 
-        return $relation->match($models,$results,$name);
+        // Once we have the results, we just match those back up to their parent models
+        // using the relationship instance. Then we just return the finished arrays
+        // of models which have been eagerly hydrated and are readied for return.
+        return $relation->match($models, $results, $name);
     }
 
 }
--- src/HasSuperRelations.php
+++ src/HasSuperRelations.php
@@ -2,8 +2,9 @@
 
 namespace Alone\EloquentSuperRelations;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
-use Illuminate\Database\Eloquent\Relations;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 trait HasSuperRelations
 {
@@ -13,44 +14,59 @@ trait HasSuperRelations
         return new EloquentBuilder($query);
     }
 
+    /**
+     * Get a relationship value from a method.
+     *
+     * @param  string  $method
+     * @return mixed
+     *
+     * @throws \LogicException
+     */
     protected function getRelationshipFromMethod($method)
     {
         /**
-         * @var $relation Relations\Relation
+         * @var  $relation  Relation
          */
         $relation = $this->$method();
-        $results  = $this->eagerLoadRelationFromModel($relation,$method,[$this]);
-        if(is_null($results))
-        {
+        $results  = $this->eagerLoadRelationFromModel($relation, $method, [$this]);
+        if(is_null($results)) {
             $results = parent::getRelationshipFromMethod($method);
         }
+
         return $results;
     }
 
-    public function eagerLoadRelationFromModel(Relations\Relation $relation,$name,array $models = null)
+    /**
+     * Get a relationship value from parent model.
+     *
+     * @param  Relation    $relation
+     * @param  string      $name  relationship name
+     * @param  array|null  $models
+     *
+     * @return mixed
+     */
+    public function eagerLoadRelationFromModel(Relation $relation, $name, array $models = null)
     {
         $results = null;
         $parent = $relation->getParent();
-        $fun = 'eagerLoad'.Str::studly($name);
-        if(method_exists($parent,$fun))
-        {
+        $method = 'eagerLoad'.Str::studly($name);
+
+        if(method_exists($parent, $method)) {
             $query = $relation->getQuery()->getQuery();
-            $from  = data_get($query,'from');
-            $where = collect(data_get($query,'wheres',[]))->reduce(function($dat,$v) use($from)
-            {
-                if(isset($v['values']) || isset($v['value']))
-                {
-                    $key = data_get($v,'column');
-                    if(Str::startsWith($key,"$from."))
-                    {
-                        $key = substr($key,strlen($from) + 1);
+            $where = array_reduce($query->wheres ?: [], function($dat, $v) use ($query) {
+                if(isset($v['values']) || isset($v['value'])) {
+                    $key = Arr::get($v, 'column');
+                    if(Str::startsWith($key, "{$query->from}.")) {
+                        $key = substr($key, strlen($query->from) + 1);
                     }
-                    $dat[$key] = data_get($v,'values',[data_get($v,'value')]);
+                    $dat[$key] = $v['values'] ?? [$v['value']];
                 }
+
                 return $dat;
             },[]);
-            $results = $parent->$fun($relation,isset($models) ? $models : [$this],$where);
+            $results = $parent->$method($relation, isset($models) ? $models : [$this], $where);
         }
+
         return $results;
     }
 
